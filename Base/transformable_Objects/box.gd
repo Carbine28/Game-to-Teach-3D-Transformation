01b50af3cc5c3fa7f758d210bfead55757bf6ee3
Wilson Ko <62252240+Carbine28@users.noreply.github.com>
2023-02-01 11:22:45 +0000

    Started Experimenting with GUI design for CM1

--- Base/transformable_Objects/box.gd
+++ Base/transformable_Objects/box.gd
@@ -6,3 +6,8 @@ var defaultSpawnPoint
 
 func _ready():
 	defaultSpawnPoint = global_translation
+
+# Each object having this script is pretty bad.
+# Should use a manager instead with IDs for each object.
+# Group each object into 1 group instead
+# Maybe consider making a class for these transformable objects?
